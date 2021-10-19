24692eac8a0bd4346e042c4705fa2da9bc35625a
Dmytro Tarasonok <dmitrij.tarasjonok@gmail.com>
2021-10-19 10:39:43 +0300

    Додано лабораторну роботу №5 (Python)

---
+++ Основы программирования/1 курс/Лабораторная работа №5/Python/main.py
@@ -0,0 +1,12 @@
+from os import system
+
+""" Варіант 29
+    Знайти всі чотирьохзначні паліндроми """
+
+for i in range(1000, 10000):
+    i_str = str(i)
+    if i_str == i_str[::-1]:
+        print(f'{i} - число-паліндром')
+
+system('pause')
+
